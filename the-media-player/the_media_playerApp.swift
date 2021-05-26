f6f27fbe3d8cabff088b05d8964d8bbea5394d79
jmt1423 <jon.thomann30@gmail.com>
2021-05-26 15:12:10 +0100

    Initial Commit

---
+++ the-media-player/the_media_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_media_playerApp.swift
+//  the-media-player
+//
+//  Created by Jonathan Thomann on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct the_media_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
